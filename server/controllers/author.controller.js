0feb2ecb63264dc40533ea4db5b124a2362db254
Miry Konishi <miry.konishi@gmail.com>
2023-02-17 13:31:09 -0300

    Backend done

--- server/controllers/author.controller.js
+++ server/controllers/author.controller.js
@@ -1,5 +1,78 @@
-const { Author } = require('../models/author.model');
+const { Author } = require("../models/author.model");
 
 module.exports.test = (req, res) => {
-  res.json({message: 'Backend is functioning'});
+  res.json({ message: "Backend is functioning" });
+};
+
+module.exports.getAllAuthors = async (req, res) => {
+  try {
+    const allAuthors = await Author.find({}).sort({name: 1});
+    res.json(allAuthors);
+  } catch (err) {
+    res.status(400);
+    res.json(err.message);
+  }
+}
+
+module.exports.createAuthor = async (req, res) => {
+  try {
+    const newAuthor = await Author.create(req.body);
+    res.json(newAuthor);
+  } catch (err) {
+    res.status(400);
+    res.json(err.message);
+  }
+};
+
+module.exports.updateAuthor = async (req, res) => {
+  try {
+    const update = await Author.findOneAndUpdate({_id: req.params._id}, req.body, { new: true, runValidators: true })
+    res.json(update);
+  } catch (err) {
+    res.status(400);
+    res.json(err.message);
+  }
+}
+
+// Delete with query.params
+// module.exports.deleteQuery = async (req, res) => {
+//   try {
+//     const deleteAuthor = await Author.findOneAndDelete(req.query);
+//     res.json({deleted: deleteAuthor})
+//   } catch (err) {
+//     res.status(400);
+//     res.json(err.message);
+//   }
+// }
+
+// Update with query.params
+// module.exports.updateQuery = async (req, res) => {
+//   try {
+//     const update = await Author.findOneAndUpdate(req.query, req.body, {new: true, runValidators: true});
+//     res.json(update);
+//   } catch (err) {
+//     res.status(400);
+//     res.json(err.message);
+//   }
+// }
+
+module.exports.deleteAuthor = async (req, res) => {
+  try {
+    const deleteAuthor = await Author.findOneAndDelete({_id: req.params._id});
+    res.json({deleted: deleteAuthor})
+  } catch (err) {
+    res.status(400);
+    res.json(err.message);
+  }
+}
+
+
+module.exports.getAuthorBy = async (req, res) => {
+  try {
+    const authors = await Author.find(req.query);
+    res.json(authors)
+  } catch (err) {
+    res.status(400);
+    res.json(err.message);
+  }
 }
--- server/routers/author.route.js
+++ server/routers/author.route.js
@@ -2,4 +2,10 @@ const AuthorController = require('../controllers/author.controller');
 
 module.exports = (app) => {
   app.get('/test', AuthorController.test);
+  app.post('/', AuthorController.createAuthor);
+  app.get('/', AuthorController.getAllAuthors);
+  app.put('/:_id', AuthorController.updateAuthor);
+  app.delete('/:_id', AuthorController.deleteAuthor);
+  // app.put('/', AuthorController.updateQuery);
+  // app.delete('/', AuthorController.deleteQuery);
 }
